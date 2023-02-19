75a47232b95e436abd3faed6b0c0df8c722815a8
Mubeen Hameed <rabimubeen955@gmail.com>
2023-02-20 02:44:27 +0500

    Add files via upload

---
+++ area.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int length,width,area,perimeter;
+	cout<<"Enter values of length,width:";
+	cin>>width>>length;
+	area=width*length;
+	perimeter=(2*width)+(2*length);
+	
+	cout<<"area ="<<area;
+	cout<<"\nperimeter="<<perimeter;
+	
+	
+}
